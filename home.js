f66ffec2f7f53b5525993461ea8d056212c4f6ec
Hardik Chadha <143736130+Hardik1106@users.noreply.github.com>
2024-01-24 22:49:58 +0530

    2nd set of changes

---
+++ home.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('darkModeSwitch').addEventListener('change', function(event) {
+        document.body.classList.toggle('dark-mode', event.target.checked);
+    });
+});
