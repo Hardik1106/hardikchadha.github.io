7ffb633c4b3ca9a119f4843da3340da8e9924bab
Hardik Chadha <143736130+Hardik1106@users.noreply.github.com>
2024-01-30 10:59:29 +0530

    Improved the dark mode switch

--- home.js
+++ home.js
@@ -1,6 +1,5 @@
 window.onload = function() {
     const darkModeSwitch = document.getElementById('darkModeSwitch');
-
     darkModeSwitch.addEventListener('change', function() {
         document.body.classList.toggle('dark-mode', this.checked);
     });
